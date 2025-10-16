0aac145c7d637f437ae863b7c6b8dc1842eaeff8
dueon <hdejh79@gmail.com>
2025-10-16 11:54:31 -0700

    I'm done

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -1,10 +1,32 @@
 // Create a function called deleteAllNodesWithValue that removes all nodes with a specific value from the DLL
 // Make sure to update the size of the list as well
 
-const DLL = require('../lib/DLL');
+const DLL = require("../lib/DLL");
 
 function deleteAllNodesWithValue(list, value) {
   // your code here
+  if (!list.head) return;
+
+  let current = list.head;
+
+  while (current) {
+    if (current.data === value) {
+      if (current.prev) {
+        current.prev.next = current.next;
+      } else {
+        list.head = current.next;
+      }
+
+      if (current.next) {
+        current.next.prev = current.prev;
+      } else {
+        list.tail = current.prev;
+      }
+
+      list.size--;
+    }
+    current = current.next;
+  }
 }
 
 const list = new DLL();
